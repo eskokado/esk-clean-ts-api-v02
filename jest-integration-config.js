c4ff55f0b7760ab302f80b28b107a29318f2e941
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-10 13:13:59 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+﻿const config = require('./jest.config')
+config.testMatch = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+﻿const config = require('./jest.config')
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
