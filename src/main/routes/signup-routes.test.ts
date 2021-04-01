60d8575983f638f3d60538dbc6da2d4c3bcc9ac4
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-01 12:00:40 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -3,27 +3,27 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
-
     await request(app)
       .post('/api/signup')
       .send({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
+        name: 'Rodrigo',
+        email: 'rodrigo.manguinho@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
       })
       .expect(200)
   })
